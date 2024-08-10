993b6f7fed48de0920f889264d8ecdc8f6a8d70c
Michael Haberler <git@mah.priv.at>
2024-08-10 14:02:35 +0200

    try neopixelWrite()

--- src/main.cpp
+++ src/main.cpp
@@ -24,36 +24,43 @@ PsychicHttpServer server;
 ESP32SvelteKit esp32sveltekit(&server, 120);
 
 LightMqttSettingsService lightMqttSettingsService = LightMqttSettingsService(&server,
-                                                                             esp32sveltekit.getFS(),
-                                                                             esp32sveltekit.getSecurityManager());
+    esp32sveltekit.getFS(),
+    esp32sveltekit.getSecurityManager());
 
 LightStateService lightStateService = LightStateService(&server,
-                                                        esp32sveltekit.getSocket(),
-                                                        esp32sveltekit.getSecurityManager(),
-                                                        esp32sveltekit.getMqttClient(),
-                                                        &lightMqttSettingsService);
+                                      esp32sveltekit.getSocket(),
+                                      esp32sveltekit.getSecurityManager(),
+                                      esp32sveltekit.getMqttClient(),
+                                      &lightMqttSettingsService);
 
 void broker_setup();
 void broker_loop();
 
-void setup()
-{
+void setup() {
     // start serial and filesystem
     Serial.begin(SERIAL_BAUD_RATE);
+
+#ifdef BUILTIN_RGBLED_PIN
+    pinMode(BUILTIN_RGBLED_PIN, OUTPUT);
+    neopixelWrite(BUILTIN_RGBLED_PIN, 0, 255, 0);
+#endif
 
     // start ESP32-SvelteKit
     esp32sveltekit.begin();
 
- broker_setup();
+    broker_setup();
 
     // load the initial light settings
     lightStateService.begin();
     // start the light service
     lightMqttSettingsService.begin();
+
+#ifdef BUILTIN_RGBLED_PIN
+    neopixelWrite(BUILTIN_RGBLED_PIN, 255, 0, 255);
+#endif
 }
 
-void loop()
-{
+void loop() {
     broker_loop();
     yield();
     // Delete Arduino loop task, as it is not needed in this example
